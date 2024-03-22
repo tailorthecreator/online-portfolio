566dc097322427e62ddfbe99f629ee4cb4884752
taishi <taishiarai@gmail.com>
2024-03-22 23:46:48 +0800

    this is my first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <HelloWorld/>
+  <h1 class="font-bold text-red-500">Red</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1 class="font-bold">Hello World</h1>
+
+</template>
+
+<style>
+
+</style>
