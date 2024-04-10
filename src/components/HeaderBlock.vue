251e6df0d41f98f0ee40f77d8281f7a2d4747f34
taishi <taishiarai@gmail.com>
2024-04-10 15:31:25 +0800

    added animation to the home page

--- src/components/HeaderBlock.vue
+++ src/components/HeaderBlock.vue
@@ -4,12 +4,4 @@
 
 <template>
 
-    <section class="sticky top-0 bg-sky-950 w-full h-20 flex justify-start items-end">
-        <ClientOnly>
-            <div class="font-bold text-gray-200 text-lg mb-5 ml-5">{{ $route.name }}</div>
-        </ClientOnly>
-    </section>
-
-
-
 </template>
--- views/Home.vue
+++ views/Home.vue
@@ -1,5 +1,5 @@
 <script setup>
-import Testimonial from "../src/components/Testimonial.vue"
+import Testimonial from "../src/components/Testimonial.vue";
 
 </script>
 
@@ -59,66 +59,56 @@ import Testimonial from "../src/components/Testimonial.vue"
 
             </div>
         </section>
+
         <div class="mx-20 lg:mx-40 my-20">
-            <div class="text-center text-2xl font-bold mb-5"> Curiousity has always been my greatest strength.</div>
-            <p class="text-center ">In many endeavors. My curiosity had lead me to the most obscure parts of this
-                planet, i believe it has taught
-                me how to not only cope but to really dive into the things that catch my interest.</p>
+            <p class="text-center text-2xl font-bold mb-5"> Change is the only thing constant.</p>
+            <p class="text-center ">I am a seasoned professional with years of experience in media production, now
+                seeking to transition into a front-end developer role. With a strong foundation in creativity, technical
+                expertise, and strategic vision, I am eager to apply my skills to elevate brands and drive growth
+                through innovative web development solutions.</p>
         </div>
         <!--The best feature Icons-->
-        <section class="bg-white dark:bg-gray-900">
+        <section class="bg-gray-900">
             <div
                 class="gap-8 items-center py-8 px-4 lg:mx-40 max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-                <img class="w-full dark:hidden"
+                <img class=" swiperight w-full dark:hidden"
                     src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg"
                     alt="dashboard image">
-                <img class="w-full hidden dark:block"
+                <img class="swiperight w-full hidden dark:block"
                     src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg"
                     alt="dashboard image">
                 <div class="mt-4 md:mt-0">
-                    <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Let's create
-                        more tools and ideas that brings us together.</h2>
-                    <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Flowbite helps you connect
-                        with friends and communities of people who share your interests. Connecting with your friends
-                        and family as well as discovering new ones is easy with features like Groups.</p>
-                    <a href="#"
-                        class="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
-                        Get started
-                        <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                clip-rule="evenodd"></path>
-                        </svg>
-                    </a>
+                    <p class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">13 years in
+                        Media Production</p>
+                    <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">From the age of 18, I
+                        immersed myself in the dynamic world of nightlife photography, honing my skills amidst the
+                        vibrant energy of nightclubs. As my expertise flourished, I transitioned to the high seas,
+                        spending a year and a half as a photographer aboard a cruise ship. At 22, driven by my passion
+                        for visual storytelling, I made the leap to Los Angeles to pursue a career in commercial and
+                        fashion photography.</p>
                 </div>
             </div>
         </section>
-        <section class="bg-white dark:bg-gray-900">
+        <section class="bg-gray-900">
             <div
                 class="gap-8 items-center py-8 px-4 lg:mx-40 max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
                 <div class="mt-4 md:mt-0 md:text-right">
                     <!-- Align content to the right for medium screens and above -->
-                    <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Let's create
-                        more tools and ideas that bring us together.</h2>
-                    <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Flowbite helps you connect
-                        with friends and communities of people who share your interests. Connecting with your friends
-                        and family as well as discovering new ones is easy with features like Groups.</p>
-                    <a href="#"
-                        class="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
-                        Get started
-                        <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                clip-rule="evenodd"></path>
-                        </svg>
-                    </a>
+                    <p class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">5+ years as
+                        a<br>
+                        Shopify Developer</p>
+                    <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">While balancing my
+                        photography work during nights and weekends, I also took on the role of Digital Marketer for a
+                        clothing line. In this capacity, I was tasked with developing an online webshop. Leveraging
+                        Shopify as my CMS, I successfully crafted a high-performing website that experienced remarkable
+                        growth. Starting from $7000 in monthly sales, the webshop flourished under my direction,
+                        reaching $150,000 in monthly sales over a span of 4 years.</p>
+
                 </div>
-                <img class="w-full dark:hidden justify-self-end"
+                <img class="swipeleft w-full dark:hidden justify-self-end"
                     src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg"
                     alt="dashboard image"> <!-- Align image to the right for dark mode and small screens -->
-                <img class="w-full hidden dark:block justify-self-end"
+                <img class="swipeleft w-full hidden dark:block justify-self-end"
                     src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg"
                     alt="dashboard image"> <!-- Align image to the right for dark mode and small screens -->
             </div>
@@ -150,5 +140,57 @@ import Testimonial from "../src/components/Testimonial.vue"
 
 .custom-shape-divider-top-1712150236 .shape-fill {
     fill: #D5D3D1;
+}
+
+p {
+    animation: fade linear both;
+    animation-timeline: view();
+    animation-range: entry 50% cover 50%;
+}
+
+@keyframes fade {
+    from {
+        opacity: 0;
+    }
+
+    to {
+        opacity: 1;
+    }
+}
+
+.swiperight {
+    animation: swipeToRight linear both;
+    animation-timeline: view();
+    animation-range: entry 10% cover 30%;
+}
+
+@keyframes swipeToRight {
+    from {
+        margin-left: -200px;
+        opacity: 0;
+    }
+
+    to {
+        margin-left: 0px;
+        opacity: 1;
+    }
+}
+
+.swipeleft {
+    animation: swipeToLeft linear both;
+    animation-timeline: view();
+    animation-range: entry 10% cover 30%;
+}
+
+@keyframes swipeToLeft {
+    from {
+        margin-right: -200px;
+        opacity: 0;
+    }
+
+    to {
+        margin-left: 0px;
+        opacity: 1;
+    }
 }
 </style>
